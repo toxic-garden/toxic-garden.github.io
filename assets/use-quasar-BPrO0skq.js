983ac1b88e4d8a7c46d3302ed4d3a04a24140f00
El Toxo <root@localhost>
2025-07-11 20:15:57 +0200

    pages for path correction....

---
+++ assets/use-quasar-BPrO0skq.js
@@ -0,0 +1,1 @@
+import{f as a,b3 as r}from"./index-CpW3-iVN.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar-BbdR9xAu.js
@@ -0,0 +1,1 @@
+import{f as a,b3 as r}from"./index-DCcCDLBa.js";function u(){return a(r)}export{u};
